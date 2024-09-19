c6656a93a71c7dc7454cc4ead7034c81a948fc0a
João Falcão <jgabfalcao@gmail.com>
2024-09-19 19:46:11 -0300

    FEAT: Inserção da Tabela Categoria

--- create_table.py
+++ create_table.py
@@ -3,12 +3,20 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
-# Criar tabela PibBrasil
 conn.execute('''
 CREATE TABLE IF NOT EXISTS PibBrasil (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     periodo TEXT UNIQUE NOT NULL,
     valor REAL NOT NULL
+);
+''')
+
+conn.execute('''
+CREATE TABLE IF NOT EXISTS Categoria (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    periodo TEXT,
+    FOREIGN KEY (periodo) REFERENCES PibBrasil(periodo)
 );
 ''')
 
